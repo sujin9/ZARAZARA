615693f21eb5ffef6b1eef7c7cab8e5bf5d28ec3
sujin9 <sujinkim919@gmail.com>
2021-05-17 01:56:53 +0900

    update layout

---
+++ speech/app/src/main/java/com/example/zarazara/RaiseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.zarazara
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RaiseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_raise)
+    }
+
+}
