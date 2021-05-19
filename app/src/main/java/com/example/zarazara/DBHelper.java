b9f484fa0c553542fd4081a1efa90d1c0b6290d8
HYE <yehh0791@gmail.com>
2021-05-19 23:30:45 +0900

    Fix 걸음 수 측정 기능

--- app/src/main/java/com/example/zarazara/DBHelper.java
+++ app/src/main/java/com/example/zarazara/DBHelper.java
@@ -21,8 +21,8 @@ public class DBHelper extends SQLiteOpenHelper {
         String sql = "CREATE TABLE IF NOT EXISTS stepStore ("
                 + "_id integer primary key autoincrement,"
                 + "steps INTEGER,"
-                + "date DATETIME DEFAULT (datetime('now','localtime')));";
-             //   + "date TEXT)";
+             //   + "date DATETIME DEFAULT (datetime('now','localtime')));";
+                + "date TEXT);";
         db.execSQL(sql);
     }
 
@@ -34,25 +34,42 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void updateDailyStep(int step) {
+    public int checkTable() {
+        SQLiteDatabase db = getWritableDatabase();
+        String sql = "SELECT count(*) FROM stepStore;";
+        Cursor cursor = db.rawQuery(sql, null);
+        cursor.moveToFirst();
+
+        // 컬럼 값이 하나 이상 있다면
+        if(cursor.getInt(0) > 0)
+            return 0;
+        else
+            return -1;
+    }
+
+    public void updateDailyStep(String today, int step) {
         SQLiteDatabase db = getWritableDatabase();
         // 입력한 항목과 일치하는 날짜(당일)의 실시간 걸음 수 업데이트
-        db.execSQL("UPDATE stepStore SET steps =" + step + " WHERE _id = (SELECT max(_id) FROM stepStore);");
+        //db.execSQL("UPDATE stepStore SET steps =" + step + " WHERE _id = (SELECT max(_id) FROM stepStore);");
+        db.execSQL("UPDATE stepStore SET steps =" + step + " WHERE date = '" + today +"';");
         db.close();
     }
 
-    public void insert(int step) {
+    public void insert(String today, int step) {
         // 읽고 쓰기가 가능하게 DB 열기
         SQLiteDatabase db = getWritableDatabase();
         // DB에 입력한 값으로 행 추가
-        db.execSQL("INSERT INTO stepStore (_id, steps) VALUES (null, " + step + ");");
+      //  db.execSQL("INSERT INTO stepStore (_id, steps) VALUES (null, " + step + today +");");
+        db.execSQL("INSERT INTO stepStore VALUES (null, " + step + ",'" + today +"');");
         db.close();
     }
 
     public int getStep(String today) {
         SQLiteDatabase db = getWritableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT SUM(steps) FROM stepStore WHERE date BETWEEN '" + today + " 00:00:00' AND '" + today + " 23:59:59';" , null);
+       // Cursor cursor = db.rawQuery("SELECT SUM(steps) FROM stepStore WHERE date BETWEEN '" + today + " 00:00:00' AND '" + today + " 23:59:59';" , null);
+        // _id>1 : 첫 실행 시 걸음 수가 올라가지 않는 부분 임시 체크
+        Cursor cursor = db.rawQuery("SELECT SUM(steps) FROM stepStore WHERE _id > 1 AND date LIKE '" + today + "%';" , null);
         cursor.moveToFirst();
         if(cursor.getCount() <= 0)
             return 0;
--- app/src/main/java/com/example/zarazara/WalkActivity.java
+++ app/src/main/java/com/example/zarazara/WalkActivity.java
@@ -86,16 +86,23 @@ public class WalkActivity extends AppCompatActivity implements SensorEventListen
         date = new Date(now);
         SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd");
         today = sdt.format(date);
-        // 새로운 instance 생성
-        save = helper.getStep(today);
-        helper.insert(0);
+
+        if(helper.checkTable() < 0)
+            helper.insert("0",0);
+        else
+        {
+            // 새로운 날짜에만 instance 생성
+            save = helper.getStep(today);
+            if (save == 0)
+                helper.insert(today, 0);
+        }
+
 
         stepCountView = findViewById(R.id.stepCountView);
         totalStepCountView = findViewById(R.id.totalStepCountView);
         todayView = findViewById(R.id.day); //
 
         todayView.setText(today); //
-
 
         // 활동 퍼미션 체크 (권한 체크)
         if(ContextCompat.checkSelfPermission(this,
@@ -115,7 +122,6 @@ public class WalkActivity extends AppCompatActivity implements SensorEventListen
             Toast.makeText(this, "No Step Sensor", Toast.LENGTH_SHORT).show();
         }
 
-
     }
 
 
@@ -133,30 +139,35 @@ public class WalkActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
+/*
+    @Override
+    public void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+*/
+
+
     // 실제 센서의 작동과 관련된 함수
     // 동작을 감지하면 이벤트를 발생하여 onSensorChanged 에 값을 전달
     @Override
     public void onSensorChanged(SensorEvent event) {
         // 걸음 센서 이벤트 발생시
         if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
-            //if(event.values[0]==1.0f){}
-
             // 초기값 설정(TYPE_STEP_COUNTER 는 스스로 초기화 x)
             if(mSteps < 1)
             {
                 mSteps = (int) event.values[0];
+                save = helper.getStep(today);
             }
-
             // 센서 이벤트가 발생할때 마다 걸음수 증가
             // 현재 값 = (리셋 안 된 값 + 현재 값) - 리셋 안 된 값 + db에 저장된 그날의 걸음수 불러와서
-            mCurrentSteps = (int)event.values[0] - mSteps;
-            helper.updateDailyStep(mCurrentSteps);
-            stepCountView.setText(String.valueOf(save + mCurrentSteps));
-
+            mCurrentSteps = (int)event.values[0] - mSteps + save;
+            helper.updateDailyStep(today, mCurrentSteps);
+            stepCountView.setText(String.valueOf(mCurrentSteps));
             // 누적값
             mTotalSteps = helper.getTotalStep();
             totalStepCountView.setText(String.valueOf(mTotalSteps));
-
         }
 
     }
