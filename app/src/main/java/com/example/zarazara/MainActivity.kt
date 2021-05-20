ced2bb58974568fb6f523c805ce5c4aeb99398e1
sujin9 <sujinkim919@gmail.com>
2021-05-20 20:33:08 +0900

    FIX merge 과정에서 누락된 코드 추가

--- app/src/main/java/com/example/zarazara/MainActivity.kt
+++ app/src/main/java/com/example/zarazara/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.zarazara
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
