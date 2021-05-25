9deb27615749ffe3bcdee5854a5ffde494d5ba3f
sujin9 <sujinkim919@gmail.com>
2021-05-26 01:07:15 +0900

    접속 날짜 변화 감지

--- app/src/main/java/com/example/zarazara/MainActivity.kt
+++ app/src/main/java/com/example/zarazara/MainActivity.kt
@@ -2,28 +2,96 @@ package com.example.zarazara
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var userCoin:Int = 0    // 유저가 보유한 코인    --> 매달 12시 추가
+    var name:String = "모찌"   // 유저 캐릭터 애칭      --> 최초 실행할 때 튜토리얼에서 입력받음
+
+
+    // 날짜 체크용 시간
+    //var currentDate:String = ""   // 가장 최근 접속했던 날짜
+    var nowDate:String = ""       // 접속한 현재 날짜
+    var now:Long = 0              // 현재 시점 불러옴
+    lateinit var date:Date
+    var sdt = SimpleDateFormat("yyyy-MM-dd")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 첫 실행 여부 확인
+        val sharedPreferences = getSharedPreferences("checkFirstAccess", MODE_PRIVATE)
+        val checkFirstAccess = sharedPreferences.getBoolean("checkFirstAccess", false)
+        val spCurrentDate = getSharedPreferences("saveCurrentDate", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        if(!checkFirstAccess) {
+            editor.putBoolean("checkFirstAccess", true)
+            editor.apply()
+
+            // 첫 접속 날짜 저장
+            now = java.lang.System.currentTimeMillis()
+            date = java.util.Date(now)
+            //currentDate = sdt.format(date)
+            nowDate = sdt.format(date)
+            editor.putString("currentDate", nowDate)
+            editor.apply()
+
+            // 튜토리얼 실행
+            //val tutorialIntent = Intent(this, TutorialActivity::class.java)
+            //startActivity(tutorialIntent)
+            //finish()
+        }
+        else {
+            // 날짜 변경 비교 --> 코인 획득
+            now = java.lang.System.currentTimeMillis()
+            date = java.util.Date(now)
+            nowDate = sdt.format(date)
+
+            Log.d("CheckDate",sharedPreferences.getString("currentDate", "Default")+"***"+nowDate)
+
+            if(checkDateChanged(sharedPreferences.getString("currentDate", "Default").toString(), nowDate)) {
+                // 다른 날짜
+                var yesterday = sdt.format(java.util.Date(now-24*60*60*1000))
+                //var newCoin: Int = (application as WalkActivity).helper.getStep(yesterday)
+                //Log.d("CheckCoin", newCoin.toString())
+                //Toast.makeText(this, "새로운 코인 "+newCoin+"을 얻었습니다", Toast.LENGTH_SHORT).show()
+
+                editor.putString("currentDate", nowDate)
+                editor.apply()
+            }
+            else {
+                // 같은 날짜
+
+            }
+
+        }
+
         // 선언
+        // 텍스트뷰
+        var nameText = findViewById<TextView>(R.id.characterName)   // 캐릭터 이름
+        var coinText = findViewById<TextView>(R.id.userCoin)        // 보유한 코인
         // 버튼
-        var homeButton1 = findViewById<ImageButton>(R.id.homeBtn1) // 키우기 버튼
-        var homeButton2 = findViewById<ImageButton>(R.id.homeBtn2) // 걸음수 버튼
-        var homeButton3 = findViewById<ImageButton>(R.id.homeBtn3) // 미션 버튼
+        var homeButton1 = findViewById<ImageButton>(R.id.homeBtn1)  // 키우기 버튼
+        var homeButton2 = findViewById<ImageButton>(R.id.homeBtn2)  // 걸음수 버튼
+        var homeButton3 = findViewById<ImageButton>(R.id.homeBtn3)  // 미션 버튼
         // intent
         var raiseIntent = Intent(this, RaiseActivity::class.java)
         var walkIntent = Intent(this, WalkActivity::class.java)
         var missionIntent = Intent(this, MissionActivity::class.java)
+
+        //
+        coinText.text = userCoin.toString()
+    //    nameText.text = name
 
         // 하단 버튼 클릭할 때
         homeButton1.setOnClickListener {
@@ -39,6 +107,7 @@ class MainActivity : AppCompatActivity() {
         homeButton3.setOnClickListener {
             startActivity(missionIntent)
         }
+
 
         //모찌말풍선이랑 경험치바 말풍선이랑 겹치지 않게, 1 = 말풍선 띄워짐, 0 = 말풍선 숨김
         var mozzi_num = 0
@@ -89,6 +158,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // 날짜 비교 체크 함수
+    fun checkDateChanged(date1:String, date2:String):Boolean {
+        if (date1==date2) {
+            return false        // 같은 날짜
+        }
+        else {
+            Toast.makeText(this, "날짜 바뀜!", Toast.LENGTH_LONG).show()
+            return true        // 다른 날짜
+        }
+    }
+
     // 알림 채널(서비스 - notification)
     // Button2(걸음 수) 기능은 Foreground Service + notification 으로 수행
     fun onStartForegroundService(view: View?) {
@@ -100,5 +180,7 @@ class MainActivity : AppCompatActivity() {
             startService(intent)
         }
     }
+
+
 
 }
