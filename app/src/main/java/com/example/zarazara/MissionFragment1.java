d5b890db6c28ecd2894a818fa3c3417a6283f23f
ky10035 <81634878+ky10035@users.noreply.github.com>
2021-05-30 23:15:27 +0900

    Update MissionFragment1.java

--- app/src/main/java/com/example/zarazara/MissionFragment1.java
+++ app/src/main/java/com/example/zarazara/MissionFragment1.java
@@ -1,11 +1,14 @@
 package com.example.zarazara;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,64 +17,130 @@ import androidx.core.app.ComponentActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
-public class MissionFragment1 extends Fragment {
+import java.util.Date;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class MissionFragment1 extends Fragment {
     View v;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_mission1,container,false);
-
         return v;
     }
 
     int count = 0; //체크박스 개수 세기, 1개라도 체크일시 00시에 0으로 초기화하고 빈박스로 변경하는 작업 진행
-    WalkActivity walkActivity = new WalkActivity(); //walkActivity에서 시간과 날짜 활용하기 위함
+    int userCoin;   //메인에서 불러온 코인값 저장
+
+    WalkActivity walkActivity = new WalkActivity(); //walkActivity에서 걸음수 활용
+    MainActivity mainActivity = new MainActivity(); //mainActivity에서 시간과 날짜 활용
 
     public void setWalkActivity(){
+        //열어주기 및 editor 사용할 수 있게
+        SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("shared", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
         if(walkActivity.mCurrentSteps >= 100 && walkActivity.mCurrentSteps < 500){
+            Toast.makeText(getActivity(), "500보 걷자", LENGTH_SHORT).show();
+
+            //말풍선 change
             v.findViewById(R.id.checkbox_x11).setVisibility(View.INVISIBLE);
             v.findViewById(R.id.checkbox_o11).setVisibility(View.VISIBLE);
+
+            //코인 값 추가 및 말풍선 개수 추가
+            userCoin = sharedPreferences.getInt("userCoin", 0) + 1;
+
+            Toast.makeText(getActivity(), "1코인이 적립되었습니다!", LENGTH_SHORT).show();
+
+            editor.putInt("userCoin", userCoin);
+            editor.apply();
+
             count++;
         }
         else if(walkActivity.mCurrentSteps >= 500 && walkActivity.mCurrentSteps < 1000){
-        //    R.id.checkbox_x12.setVisibility(View.INVISIBLE);
-        //    R.id.checkbox_o12.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(), "1000보 걷자", LENGTH_SHORT).show();
+
+            v.findViewById(R.id.checkbox_x12).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o12).setVisibility(View.VISIBLE);
+
+            userCoin = sharedPreferences.getInt("userCoin", 0) + 5;
+
+            Toast.makeText(getActivity(), "5코인이 적립되었습니다!", LENGTH_SHORT).show();
+
+            editor.putInt("userCoin", userCoin);
+            editor.apply();
             count++;
         }
         else if(walkActivity.mCurrentSteps >= 1000 && walkActivity.mCurrentSteps < 5000){
-        //    R.id.checkbox_x13.setVisibility(View.INVISIBLE);
-        //    R.id.checkbox_o13.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(), "5000보 걷자", LENGTH_SHORT).show();
+
+            v.findViewById(R.id.checkbox_x13).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o13).setVisibility(View.VISIBLE);
+
+            userCoin = sharedPreferences.getInt("userCoin", 0) + 20;
+
+            Toast.makeText(getActivity(), "20코인이 적립되었습니다!", LENGTH_SHORT).show();
+
+            editor.putInt("userCoin", userCoin);
+            editor.apply();
+
             count++;
         }
         else if(walkActivity.mCurrentSteps >= 5000 && walkActivity.mCurrentSteps < 10000){
-        //    R.id.checkbox_x14.setVisibility(View.INVISIBLE);
-        //    R.id.checkbox_o14.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(), "10000보 걷자", LENGTH_SHORT).show();
+
+            v.findViewById(R.id.checkbox_x14).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o14).setVisibility(View.VISIBLE);
+
+            userCoin = sharedPreferences.getInt("userCoin", 0) + 60;
+
+            Toast.makeText(getActivity(), "60코인이 적립되었습니다!", LENGTH_SHORT).show();
+
+            editor.putInt("userCoin", userCoin);
+            editor.apply();
+
             count++;
         }
         else{
-        //    R.id.checkbox_x15.setVisibility(View.INVISIBLE);
-        //    R.id.checkbox_o15.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(), "오늘 할일 끝!", LENGTH_SHORT).show();
+
+            v.findViewById(R.id.checkbox_x15).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o15).setVisibility(View.VISIBLE);
+
+            userCoin = sharedPreferences.getInt("userCoin", 0) + 120;
+
+            Toast.makeText(getActivity(), "120코인이 적립되었습니다!", LENGTH_SHORT).show();
+
+            editor.putInt("userCoin", userCoin);
+            editor.apply();
+
             count++;
         }
 
-        /*
-        if(walkActivity. && count >= 1){ //00시 되면 체크박스 초기화, 시간변수 어떻게 사용?
-            R.id.checkbox_o11.setVisibility(View.INVISIBLE);
-            R.id.checkbox_o12.setVisibility(View.INVISIBLE);
-            R.id.checkbox_o13.setVisibility(View.INVISIBLE);
-            R.id.checkbox_o14.setVisibility(View.INVISIBLE);
-            R.id.checkbox_o15.setVisibility(View.INVISIBLE);
+        //날짜 불러오기
+        String date1 = sharedPreferences.getString("currentDate", "Deafault");
+        String date2 = mainActivity.getNowDate();
 
-            R.id.checkbox_x11.setVisibility(View.VISIBLE);
-            R.id.checkbox_x12.setVisibility(View.VISIBLE);
-            R.id.checkbox_x13.setVisibility(View.VISIBLE);
-            R.id.checkbox_x14.setVisibility(View.VISIBLE);
-            R.id.checkbox_x15.setVisibility(View.VISIBLE);
+        Boolean date_result = mainActivity.checkDateChanged(date1, date2);
+        if(date_result == true && count >= 1) { //00시 되면 체크박스 초기화
+            //check표시된 박스
+            v.findViewById(R.id.checkbox_o11).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o12).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o13).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o14).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.checkbox_o15).setVisibility(View.INVISIBLE);
+
+            //빈박스로 초기화
+            v.findViewById(R.id.checkbox_x11).setVisibility(View.VISIBLE);
+            v.findViewById(R.id.checkbox_x12).setVisibility(View.VISIBLE);
+            v.findViewById(R.id.checkbox_x13).setVisibility(View.VISIBLE);
+            v.findViewById(R.id.checkbox_x14).setVisibility(View.VISIBLE);
+            v.findViewById(R.id.checkbox_x15).setVisibility(View.VISIBLE);
+
             //초기화
             count = 0;
         }
-         */
     }
 }
